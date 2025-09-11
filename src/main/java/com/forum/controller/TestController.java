f5cd2aeff2234bd96eba165b85fda6f86a04c8f3
Tom <jiangchenqing07@163.com>
2025-09-11 08:28:06 +0800

    disable MySQL

--- src/main/java/com/forum/controller/TestController.java
+++ src/main/java/com/forum/controller/TestController.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DashLivingController {
-
+public class TestController {
     @GetMapping("/test")
     public String test(){
         return "test succeed";
